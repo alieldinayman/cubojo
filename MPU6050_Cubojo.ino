ee767327ea0f2e9219433248f951664b3e76cae4
Alieldin Ayman <31348348+alieldinayman@users.noreply.github.com>
2018-07-19 20:55:20 +0200

    Initial commit
    Cubojo is a 3D hack-and-slash arcade game, that makes use of motion sensing, the game captures the player’s hand movements, and translates them onto a katana sword slashing through waves of cubes thrown at you in a random fashion.
    
    <blockquote class="imgur-embed-pub" lang="en" data-id="a/DtsvKQb"><a href="//imgur.com/DtsvKQb"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
    
    Developed in Unity by interfacing C# and Arduino, and functioning on the InvenSense MPU-6050 Accelerometer and Gyro.

---
+++ MPU6050_Cubojo.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+int16_t ax, ay, az, gx, gy, gz;
+int vx, vy;
+
+void setup() {
+  Serial.begin(4800);
+  Wire.begin();
+  mpu.initialize();
+  //if (!mpu.testConnection()) { while (1); }
+}
+
+void loop() {
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  vx = ((gx+300)/150)-1;  // "+300" because the x axis of gyroscope give values about -350 while it's not moving. Change this value if you get something different using the TEST code, chacking if there are values far from zero.
+  vy = -(gz-100)/150; // same here about "-100"
+
+  String output = String(gx) + ";" + String(gz) + ";" + String(vx) + ";" + String(vy);
+
+  Serial.println(output);
+  Serial.flush();
+  
+  delay(10);
+}
---
+++ MotionSensor.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO.Ports;
+ 
+ 
+public class MotionSensor : MonoBehaviour
+{
+ 
+    SerialPort stream;
+ 
+    public GameObject target; // is the gameobject to
+ 
+    public float acc_normalizer_factor = 0.00025f;
+    public float gyro_normalizer_factor = 1.0f / 32768.0f;   // 32768 is max value captured during test on imu
+ 
+    //float curr_angle_x = 0;
+    //float curr_angle_y = 0;
+    //float curr_angle_z = 0;
+ 
+    float curr_offset_x = 0;
+    float curr_offset_y = -2;
+ 
+    // Increase the speed/influence rotation
+    public float factor = 7;
+    public bool enableRotation;
+    public bool enableTranslation;
+ 
+    // SELECT YOUR COM PORT AND BAUDRATE
+    string port = "COM5";
+    int baudrate = 4800;
+ 
+    void Start()
+    {
+		Debug.Log("OK1");
+        stream = new SerialPort("\\\\.\\" + port, baudrate);
+		
+        stream.Open();
+		Debug.Log("OK");
+    }
+ 
+    void Update()
+    {
+		/*if (curr_offset_x < -10)
+		{
+			curr_offset_x = -10;
+		}
+		
+		if (curr_offset_x > 10)
+		{
+			curr_offset_x = 10;
+		}
+		
+		if (curr_offset_y > 10)
+		{
+			curr_offset_y = 10;
+		}
+		
+		if (curr_offset_y < 1)
+		{
+			curr_offset_y = -1;
+		}*/
+		
+		if (Input.GetKeyDown("space"))
+		{
+			Debug.Log("OK");
+			curr_offset_x = 0;
+			curr_offset_y = -2;
+		}
+        string dataString = "null received";
+		dataString = stream.ReadLine();
+		
+        char splitChar = ';';
+        string[] dataRaw = dataString.Split(splitChar);
+			
+		float vx = Int32.Parse(dataRaw[2]);
+		float vy = Int32.Parse(dataRaw[3]);
+		
+        curr_offset_x += (vx*acc_normalizer_factor);
+        curr_offset_y += (vy*acc_normalizer_factor);
+		
+		//Debug.Log(curr_offset_x);
+		//Debug.Log(curr_offset_y);
+
+        if(enableTranslation) target.transform.position = new Vector3(-curr_offset_x, -curr_offset_y, -15f);
+        //if(enableRotation)target.transform.rotation = Quaternion.Euler(curr_angle_x * factor, -curr_angle_z * factor, curr_angle_y * factor);
+			
+    }
+	
+	  void OnTriggerEnter(Collider col){
+      if(col.gameObject.tag == "Cube"){
+           Destroy(col.gameObject);
+		}
+	  }
+		  
+ 
+}
